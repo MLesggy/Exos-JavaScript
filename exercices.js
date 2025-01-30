8b318f593dcf3d929b1ddc67789a5ea1e0a59864
Mélanie <melanie.taussat@laposte.net>
2025-01-30 12:50:07 +0100

    Exercices 3: condition if

--- exercices.js
+++ exercices.js
@@ -12,3 +12,16 @@ let calculette=prompt("entrez votre calcul")
 let result=eval(calculette)
 console.log(result)
 
+// Exercice 3 
+
+// Condition IF
+let note=85
+if (note >=90){
+   console.log("A");
+}else if(note >=80){
+   console.log("B");
+}else if (note>=70){
+   console.log("C");
+}else{
+   console.log("D");
+}
