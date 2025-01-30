97339d83521192510de9e21cca439900e053969c
Mélanie <melanie.taussat@laposte.net>
2025-01-30 12:48:35 +0100

    Exercice 2: projet calculatrice

--- exercices.js
+++ exercices.js
@@ -4,4 +4,11 @@
 let prenom=prompt("Saississez votre prénom")
 console.log("Bonjour "+ prenom)
 
-// 
+
+// Exercice 2
+
+//Projet calculatrice
+let calculette=prompt("entrez votre calcul")
+let result=eval(calculette)
+console.log(result)
+
